ca73840f1492bd4314d94bf0eb4bdc2860b5b6b8
tiggerdine <tiggerdine@gmail.com>
2021-01-08 14:33:40 +0100

    Rest Patterns and Parameters

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -37,6 +37,11 @@ const restaurant = {
 
     orderPasta: function (ing1, ing2, ing3) {
         console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+    },
+
+    orderPizza: function (mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
     }
 };
 
@@ -119,8 +124,8 @@ restaurant.orderDelivery({
     address: 'Via del Sole, 21',
     starterIndex: 1
 });
- */
 
+// THE SPREAD OPERATOR (...)
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
 console.log(badNewArr);
@@ -168,3 +173,46 @@ const restaurantCopy = {...restaurant};
 restaurantCopy.name = 'Ristorante Roma';
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
+ */
+
+// REST PATTERN AND PARAMETERS
+// 1) Destructuring
+
+// SPREAD, because on RIGHt side of =
+const arr = [1, 2, ...[3, 4]];
+
+// REST, because on LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+    ...restaurant.mainMenu,
+    ...restaurant.starterMenu
+];
+console.log(pizza, risotto, otherFood);
+
+// Objects
+const {sat, ...weekdays} = restaurant.openingHours;
+console.log(sat, weekdays);
+
+// 2) Functions
+const add = function(...numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
+
+// The spread operator is used where we would otherwise write VALUES separated by commas
+// The rest pattern is used where we would otherwise write VARIABLE NAMES separated by commas
