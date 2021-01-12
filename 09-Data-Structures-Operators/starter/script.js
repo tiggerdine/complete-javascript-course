49dab238977614a8159c3cb331366afb89ef01b2
tiggerdine <tiggerdine@gmail.com>
2021-01-12 20:59:07 +0100

    Maps: Fundamentals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -35,7 +35,7 @@ const restaurant = {
                       mainIndex = 0,
                       time = '20:00',
                       address
-    }) {
+                  }) {
         console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
     },
 
@@ -445,7 +445,6 @@ for (const player of game.scored) {
     }
 }
 console.log(scorers);
-*/
 
 // SETS
 const ordersSet = new Set(['Pasta', 'Pizza', 'Pizza', 'Risotto', 'Pasta', 'Pizza']);
@@ -474,3 +473,36 @@ console.log(staffUnique);
 console.log(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']);
 
 console.log(new Set('martintiggerdine').size);
+*/
+
+// Maps: Fundamentals
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+    .set('open', 11)
+    .set('close', 23)
+    .set(true, 'We are open :D')
+    .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
