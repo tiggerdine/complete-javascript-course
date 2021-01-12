78e90bf9180060f98ad8aee911e8f6b03dadcb05
tiggerdine <tiggerdine@gmail.com>
2021-01-12 21:26:47 +0100

    Coding Challenge #3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -505,7 +505,6 @@ console.log(rest);
 console.log(rest.size);
 
 console.log(rest.get(arr));
-*/
 
 // Maps: Iteration
 
@@ -543,3 +542,37 @@ console.log([...question]);
 // console.log(question.entries());
 console.log([...question.keys()]);
 console.log([...question.values()]);
+*/
+
+// Coding Challenge #3
+
+const gameEvents = new Map([
+    [17, '⚽ GOAL'],
+    [36, '🔁 Substitution'],
+    [47, '⚽ GOAL'],
+    [61, '🔁 Substitution'],
+    [64, '🔶 Yellow card'],
+    [69, '🔴 Red card'],
+    [70, '🔁 Substitution'],
+    [72, '🔁 Substitution'],
+    [76, '⚽ GOAL'],
+    [80, '⚽ GOAL'],
+    [92, '🔶 Yellow card'],
+]);
+
+// 1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3.
+console.log(`An event happened, on average, every ${90 / gameEvents.size} minutes`);
+
+// 4.
+for (const [minute, event] of gameEvents) {
+    const half = `${minute <= 45 ? 'FIRST' : 'SECOND'}`;
+    console.log(`[${half} HALF] ${minute}: ${event}`);
+}
