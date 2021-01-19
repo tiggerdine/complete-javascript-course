076b71ebbbbd663ee61be35628b285d3cb159d26
tiggerdine <tiggerdine@gmail.com>
2021-01-19 20:32:08 +0100

    153. Coding Challenge #3

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -278,7 +278,6 @@ const calcAverageHumanAge = function (ages) {
 
 console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
 console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
-*/
 
 // 152. The Magic of Chaining Methods
 
@@ -294,3 +293,14 @@ const totalDepositsUSD = movements
   })
   .reduce((acc, mov) => acc + mov);
 console.log(totalDepositsUSD);
+*/
+
+// 153. Coding Challenge #3
+
+const calcAverageHumanAge = ages => ages
+  .map(age => age <= 2 ? 2 * age : 16 + 4 * age)
+  .filter(age => age >= 18)
+  .reduce((acc, age, _, arr) => acc + age / arr.length, 0);
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
