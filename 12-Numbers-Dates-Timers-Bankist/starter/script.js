a25b69c2387aeaa67786295c4243ff46834143fb
tiggerdine <tiggerdine@gmail.com>
2021-01-21 21:39:37 +0100

    168. The Remainder Operator

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -290,7 +290,6 @@ console.log(Number.isFinite(23 / 0));
 console.log(Number.isInteger(23));
 console.log(Number.isInteger(23.0));
 console.log(Number.isInteger(23 / 0));
-*/
 
 // 167. Math and Rounding
 
@@ -332,3 +331,36 @@ console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
 console.log(+(2.345).toFixed(2));
+*/
+
+// 168. The Remainder Operator
+
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', () => {
+  [...document.querySelectorAll('.movements__row')].forEach((row, i) => {
+    // 0, 2, 4, 6
+    if (i % 2 === 0) {
+      row.style.backgroundColor = 'orangered';
+    }
+    // 0, 3, 6, 9
+    if (i % 3 === 0) {
+      row.style.backgroundColor = 'blue';
+    }
+  })
+});
