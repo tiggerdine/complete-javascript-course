5508f419e209a5553d8cfc87e458594828f11519
tiggerdine <tiggerdine@gmail.com>
2021-02-05 16:03:43 +0100

    220. Encapsulation: Protected Properties and Methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -393,28 +393,33 @@ class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
+    this._pin = pin;
+    // Protected property
+    this._movements = [];
     this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
   // Public interface
+  getMovements() {
+    return this._movements;
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
 
-  approveLoan(val) {
+  _approveLoan(val) {
     return true;
   }
 
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
     }
@@ -426,7 +431,10 @@ const acc1 = new Account('Jonas', 'EUR', 1111);
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
-acc1.approveLoan(1000);
 
 console.log(acc1);
-console.log(acc1.pin);
+console.log(acc1._pin);
+
+// 220. Encapsulation: Protected Properties and Methods
+
+console.log(acc1.getMovements());
