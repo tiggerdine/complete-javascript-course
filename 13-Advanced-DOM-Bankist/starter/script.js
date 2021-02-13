d096a5fd9a8a821e0552c956b6b64eaeac4d1fa4
tiggerdine <tiggerdine@gmail.com>
2021-02-13 17:40:25 +0100

    186. Types of Events and Event Handlers

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -30,6 +30,44 @@ document.addEventListener('keydown', function(e) {
   }
 });
 
+// 185. Implementing Smooth Scrolling
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', e => {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log(
+    'Current scroll (X/Y)',
+    window.pageXOffset,
+    window.pageYOffset
+  );
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth'
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
 ///////////////////////////////////////
 ///////////////////////////////////////
 ///////////////////////////////////////
@@ -119,40 +157,18 @@ logo.classList.contains('c'); // not includes
 logo.className = 'jonas';
 */
 
-// 185. Implementing Smooth Scrolling
+// 186. Types of Events and Event Handlers
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+const h1 = document.querySelector('h1');
 
-btnScrollTo.addEventListener('click', e => {
-  const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
+const alertH1 = () => {
+  alert('addEventListener: Great! You are reading the heading :D');
+};
 
-  console.log(e.target.getBoundingClientRect());
+h1.addEventListener('mouseenter', alertH1);
 
-  console.log(
-    'Current scroll (X/Y)',
-    window.pageXOffset,
-    window.pageYOffset
-  );
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 
-  console.log(
-    'height/width viewport',
-    document.documentElement.clientHeight,
-    document.documentElement.clientWidth
-  );
-
-  // Scrolling
-  // window.scrollTo(
-  //   s1coords.left + window.pageXOffset,
-  //   s1coords.top + window.pageYOffset
-  // );
-
-  // window.scrollTo({
-  //   left: s1coords.left + window.pageXOffset,
-  //   top: s1coords.top + window.pageYOffset,
-  //   behavior: 'smooth'
-  // });
-
-  section1.scrollIntoView({ behavior: 'smooth' });
-});
+// h1.onmouseenter = () => {
+//   alert('addEventListener: Great! You are reading the heading :D');
+// };
