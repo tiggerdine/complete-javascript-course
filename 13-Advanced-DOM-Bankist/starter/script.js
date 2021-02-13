306c7c8999b0eb86f9ce77c4bd63a0e551fa0e6f
tiggerdine <tiggerdine@gmail.com>
2021-02-13 18:32:37 +0100

    188. Event Propagation in Practice

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -155,7 +155,6 @@ logo.classList.contains('c'); // not includes
 
 // Don't use
 logo.className = 'jonas';
-*/
 
 // 186. Types of Events and Event Handlers
 
@@ -172,3 +171,29 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 // h1.onmouseenter = () => {
 //   alert('addEventListener: Great! You are reading the heading :D');
 // };
+*/
+
+// 188. Event Propagation in Practice
+
+// rgb(255,255,255)
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function(e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // Stop propagation
+  // e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function(e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener('click', function(e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
+});
