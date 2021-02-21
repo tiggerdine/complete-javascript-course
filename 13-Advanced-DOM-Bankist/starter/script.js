f491d7f136d552b16a2955acfc42ce4a610bc9a9
tiggerdine <tiggerdine@gmail.com>
2021-02-21 19:00:29 +0100

    199. Lifecycle DOM Events

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -489,3 +489,15 @@ console.log(h1.parentElement.children);
   }
 });
 */
+
+// 199. Lifecycle DOM Events
+
+document.addEventListener('DOMContentLoaded', e => console.log('HTML parsed and DOM tree built!', e));
+
+window.addEventListener('load', e => console.log('Page fully loaded', e));
+
+// window.addEventListener('beforeunload', e => {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// });
