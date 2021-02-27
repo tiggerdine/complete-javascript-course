a17c4dac1a8cb7015e7da924f1ff7b235aef6abe
tiggerdine <tiggerdine@gmail.com>
2021-02-27 21:07:22 +0100

    231. Displaying a Map Marker

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -28,6 +28,25 @@ navigator.geolocation.getCurrentPosition(
       .addTo(map)
       .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
       .openPopup();
+
+    map.on('click', mapEvent => {
+      console.log(mapEvent);
+      const { lat, lng } = mapEvent.latlng;
+
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup'
+          })
+        )
+        .setPopupContent('Workout')
+        .openPopup();
+    });
   },
   () => alert('Could not get your position')
 );
