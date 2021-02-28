c4b6ee17c953f4f05250d72523f8dbd9502a783a
tiggerdine <tiggerdine@gmail.com>
2021-02-28 20:37:23 +0100

    246. Welcome to Callback Hell

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -7,17 +7,9 @@ const countriesContainer = document.querySelector('.countries');
 
 // 244. Our First AJAX Call: XMLHttpRequest
 
-const getCountryData = country => {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
-  request.send();
-
-  request.addEventListener('load', function() {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-
-    const html = `
-  <article class='country'>
+const renderCountry = (data, className = '') => {
+  const html = `
+  <article class='country ${className}'>
     <img class='country__img' src='${data.flag}' />
     <div class='country__data'>
       <h3 class='country__name'>${data.name}</h3>
@@ -29,11 +21,58 @@ const getCountryData = country => {
   </article>
   `;
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = country => {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function() {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country (2)
+    const [neighbour] = data.borders;
+
+    if (!neighbour) {
+      return;
+    }
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function() {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('portugal');
-getCountryData('usa');
-getCountryData('germany');
+// getCountryAndNeighbour('portugal');
+getCountryAndNeighbour('usa');
+
+// 246. Welcome to Callback Hell
+
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 seconds passed');
+      setTimeout(() => {
+        console.log('4 seconds passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
