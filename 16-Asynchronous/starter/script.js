8287fcde2438ce606176c5d7b830f6a59cfccce0
tiggerdine <tiggerdine@gmail.com>
2021-03-01 14:57:59 +0100

    249. Chaining Promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -81,11 +81,23 @@ setTimeout(() => {
 
 // 247. Promises and the Fetch API
 // 248. Consuming Promises
+// 249. Chaining Promises
 
 const getCountryData = country => {
+  // Country 1
   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
 
 getCountryData('portugal');
