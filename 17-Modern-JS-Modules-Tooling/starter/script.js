44fd5aa1962f9ad279122e1f038994c94df739f4
tiggerdine <tiggerdine@gmail.com>
2021-03-10 22:08:52 +0100

    269. The Module Pattern

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,3 +1,4 @@
+/*
 // Importing module
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 // addToCart('bread', 5);
@@ -19,3 +20,33 @@ add('bread', 5);
 add('apples', 4);
 
 console.log(cart);
+*/
+
+const ShoppingCart2 = (() => {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = (product, quantity) => {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost})`);
+  };
+
+  const orderStock = (product, quantity) => {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
